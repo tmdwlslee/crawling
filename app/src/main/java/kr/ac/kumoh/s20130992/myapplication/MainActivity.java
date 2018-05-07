82048753b282505a11f3d57dd1ae97fc53a017c7
tmdwlslee <39025676+tmdwlslee@users.noreply.github.com>
2018-05-07 21:40:34 +0900

    first commit

---
+++ app/src/main/java/kr/ac/kumoh/s20130992/myapplication/MainActivity.java
@@ -0,0 +1,73 @@
+package kr.ac.kumoh.s20130992.myapplication;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private String htmlPageUrl = "http://news.naver.com/"; //파싱할 홈페이지의 URL주소
+    private TextView textviewHtmlDocument;
+    private String htmlContentInStringFormat="";
+
+    int cnt=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        textviewHtmlDocument = (TextView)findViewById(R.id.textView);
+        textviewHtmlDocument.setMovementMethod(new ScrollingMovementMethod()); //스크롤 가능한 텍스트뷰로 만들기
+
+        Button htmlTitleButton = (Button)findViewById(R.id.button);
+        htmlTitleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
+                jsoupAsyncTask.execute();
+            }
+        });
+    }
+
+    private class JsoupAsyncTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+
+                Document doc = Jsoup.connect(htmlPageUrl).get();
+
+                Elements titles= doc.select("li a");
+                for(Element e: titles){
+                    htmlContentInStringFormat += e.text().trim() + "\n";
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            textviewHtmlDocument.setText(htmlContentInStringFormat);
+        }
+    }
+
+
+}
+
+
